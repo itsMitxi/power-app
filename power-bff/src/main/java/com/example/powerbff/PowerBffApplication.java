a4216ede4687b0d0ed5f4ef41c19a9682fa1ee42
guillem.rodriguez <guillem.rm.dev@gmail.com>
2024-08-21 22:48:35 +0200

    Initial commit with initial project structure

---
+++ power-bff/src/main/java/com/example/powerbff/PowerBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.powerbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerBffApplication.class, args);
+	}
+
+}
---
+++ power-bff/src/test/java/com/example/powerbff/PowerBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.powerbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
