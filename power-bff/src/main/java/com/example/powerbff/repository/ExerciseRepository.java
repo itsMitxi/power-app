79f4f29fc64a00e3557f1a83d2b659125ba6af51
Guillem RM <guillem.rm.dev@gmail.com>
2024-08-22 02:53:13 +0200

    Bff/feature/exercises (#5)
    * Added volume to persist power-bff data
    
    * Added users entity with tests
    
    * Added users repository with tests
    
    * Added users service with some exceptions and tests
    
    * Added starter security config to permit all
    
    * Added users controller with tests and endpoints stored in constants
    
    * Added exercises entity with tests
    
    * Added exercises repository with tests

---
+++ power-bff/src/main/java/com/example/powerbff/repository/ExerciseRepository.java
@@ -0,0 +1,14 @@
+package com.example.powerbff.repository;
+
+import com.example.powerbff.entity.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+    Optional<Exercise> findByName(String name);
+
+}
