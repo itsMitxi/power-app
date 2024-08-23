6ff200fa70cf1644bad62a301d7ebb3224205c25
Guillem RM <guillem.rm.dev@gmail.com>
2024-08-23 21:08:21 +0200

    Added all fields to the exercises entity

--- power-bff/src/main/java/com/example/powerbff/entity/Exercise.java
+++ power-bff/src/main/java/com/example/powerbff/entity/Exercise.java
@@ -3,6 +3,8 @@ package com.example.powerbff.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 @Entity
 @Table(name = "exercises")
@@ -16,7 +18,18 @@ public class Exercise {
     private User createdBy;
     @Column(nullable = false)
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type")
+    private ExerciseType type;
     private String description;
     private String imgUrl;
+
+    @ManyToMany
+    @JoinTable(
+            name = "exercise_muscles",
+            joinColumns = @JoinColumn(name = "exercise_id"),
+            inverseJoinColumns = @JoinColumn(name = "muscle_id")
+    )
+    private Set<Muscle> muscles;
 
 }
