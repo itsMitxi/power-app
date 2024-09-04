d5aa22e976c3eef9f01cfeda360632cdda5140dc
Guillem RM <guillem.rm.dev@gmail.com>
2024-09-04 21:07:39 +0200

    Initial auth screen to login or register, with initial palette and api service (#23)

---
+++ power/lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFF6F00);
+  static const Color secondary = Color(0xFFFFA000);
+  static const Color background = Color(0xFF121212);
+  static const Color surface = Color(0xFF1E1E1E);
+  static const Color error = Color(0xFFCF6679);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onPrimaryDarker = Color.fromARGB(150, 255, 255, 255);
+  static const Color onSecondary = Color(0xFF000000);
+  static const Color onBackground = Color(0xFFE0E0E0);
+  static const Color onSurface = Color(0xFFE0E0E0);
+}
